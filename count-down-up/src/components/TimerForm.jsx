dbc77162e674c80242a30dfea41f1f641ee10474
KwameSei <nathanielosei92@gmail.com>
2023-10-11 12:04:29 +0000

    TimerForm code commentted out

--- count-down-up/src/components/TimerForm.jsx
+++ count-down-up/src/components/TimerForm.jsx
@@ -1,52 +1,52 @@
-import React from 'react';
+// import React from 'react';
 
-const TimerForm = ({ formData, index, handleInputChange }) => {
-  const { hours, minutes, seconds } = formData;
+// const TimerForm = ({ formData, index, handleInputChange }) => {
+//   const { hours, minutes, seconds } = formData;
 
-  const handleHoursChange = (e) => {
-    handleInputChange('hours', parseInt(e.target.value, 10));
-  };
+//   const handleHoursChange = (e) => {
+//     handleInputChange('hours', parseInt(e.target.value, 10));
+//   };
 
-  const handleMinutesChange = (e) => {
-    handleInputChange('minutes', parseInt(e.target.value, 10));
-  };
+//   const handleMinutesChange = (e) => {
+//     handleInputChange('minutes', parseInt(e.target.value, 10));
+//   };
 
-  const handleSecondsChange = (e) => {
-    handleInputChange('seconds', parseInt(e.target.value, 10));
-  };
+//   const handleSecondsChange = (e) => {
+//     handleInputChange('seconds', parseInt(e.target.value, 10));
+//   };
 
-  return (
-    <div className="timer-form">
-      <h3>Timer {index + 1}</h3>
-      <div className="form-group">
-        <label htmlFor="hours">Hours</label>
-        <input
-          type="number"
-          id="hours"
-          value={hours}
-          onChange={handleHoursChange}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="minutes">Minutes</label>
-        <input
-          type="number"
-          id="minutes"
-          value={minutes}
-          onChange={handleMinutesChange}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="seconds">Seconds</label>
-        <input
-          type="number"
-          id="seconds"
-          value={seconds}
-          onChange={handleSecondsChange}
-        />
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="timer-form">
+//       <h3>Timer {index + 1}</h3>
+//       <div className="form-group">
+//         <label htmlFor="hours">Hours</label>
+//         <input
+//           type="number"
+//           id="hours"
+//           value={hours}
+//           onChange={handleHoursChange}
+//         />
+//       </div>
+//       <div className="form-group">
+//         <label htmlFor="minutes">Minutes</label>
+//         <input
+//           type="number"
+//           id="minutes"
+//           value={minutes}
+//           onChange={handleMinutesChange}
+//         />
+//       </div>
+//       <div className="form-group">
+//         <label htmlFor="seconds">Seconds</label>
+//         <input
+//           type="number"
+//           id="seconds"
+//           value={seconds}
+//           onChange={handleSecondsChange}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default TimerForm;
+// export default TimerForm;
